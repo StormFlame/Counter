6b483c8bbafbd75e30aae57241b9fc0cd62a70f4
StormFlame <78341412+StormFlame@users.noreply.github.com>
2021-02-26 15:16:01 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+//Constants
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const resetBtn = document.getElementById('reset');
+const input = document.getElementById('input');
+const displayEl = document.getElementById('display');
+
+//state
+let total;
+let inputInt;
+
+//Event Listeners
+addBtn.addEventListener('click', function(e){
+    add(parseInt(input.value));
+});
+
+subBtn.addEventListener('click', function(e){
+    sub(parseInt(input.value));
+});
+
+resetBtn.addEventListener('click', function(e){
+    init();
+});
+
+//call init
+init();
+
+//-------------------------------------------------------- FUNCTIONS
+
+//INITIALIZE
+function init(){
+    total = 0;
+    inputInt = 0;
+    input.value = '1';
+    render();
+}
+
+//RENDER
+function render(){
+
+    displayEl.innerHTML = total;
+
+    if(total >= 0){
+        displayEl.style.color = 'black';
+    }else{
+        displayEl.style.color = 'red';
+    }
+}
+
+function renderInputError(){
+    displayEl.innerHTML = 'Input must be a number';
+
+    setTimeout(function(){
+        render();
+    }, 1000);
+}
+
+//STATE FUNCTIONS
+function add(inpt){
+
+    if(Number.isNaN(inpt)){
+        renderInputError();
+    }else{
+        total += inpt
+        render();
+    }
+}
+
+function sub(inpt){
+
+    if(Number.isNaN(inpt)){
+        renderInputError();
+    }else{
+        total -= inpt
+        render();
+    }
+}
+
+//-----------------------------------------------------------------
